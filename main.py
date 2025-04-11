e460fb0d204270a664d43ef128783c472d687e87
Skxlu <131553988+Skxlu@users.noreply.github.com>
2025-04-11 22:26:42 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import discord, os
+from discord.ext import commands
+
+intents = discord.Intents.default()
+
+bot = discord.Bot(intent=intents, debug_guilds=[1242583846139068537,1242583846139068537])
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        await ctx.send("Error")
+    elif isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Error")
+    elif isinstance(error, discord.errors.DiscordServerError):
+        await ctx.send("Error")
+    else:
+        await ctx.send("unexpected error")
+        print(f"Error: {error}")
+
+
+if __name__ == '__main__':
+    for filename in os.listdir("cogs"):
+        if filename.endswith(".py"):
+            bot.load_extension(f"cogs.{filename[:-3]}")
+
+    bot.run('TOKEN')
 
